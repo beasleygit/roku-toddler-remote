1882a04d390e8fd909b8170106c77dd4852cd5ee
Michael Beasley <beasleybytes@gmail.com>
2024-01-17 12:02:18 -0500

    added timeout screen to get initial request time to load - toddlers >.<

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,34 @@
 "use client"
 import { videos } from './posts/videos'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+  const [isClassActive, setIsClassActive] = useState(false)
+  const [countdown, setCountdown] = useState(0)
+  
+  useEffect(() => {
+    let interval;
+    if (isClassActive && countdown > 0) {
+      interval = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1);
+      }, 1000)
+    }
+    return () => clearInterval(interval);
+  }, [isClassActive, countdown]);
 
   const handleClick = async (index) => {
     const arrayLength = videos[index].length
     const randomVid =  Math.floor(Math.random() * (arrayLength - 1)) + 1
     const video = videos[index][randomVid]
-    console.log(video)
 
+    setIsClassActive(true)
+    setCountdown(15)
+
+    // Deactivate the class after 20 seconds
+    setTimeout(() => {
+      setIsClassActive(false);
+    }, 15000) // 20000 milliseconds = 20 seconds
+  
     try {
       const response = await fetch(`/api/?slug=${video}`, { method: 'POST' })
       if (!response.ok) {
@@ -16,6 +36,8 @@ export default function Home() {
       }
       // Handle success
       console.log('Request sent successfully')
+
+
     } catch (error) {
       console.error('Error fetching data:', error)
       // Handle error
@@ -24,7 +46,12 @@ export default function Home() {
 
   return (
     <main className="bg-blue-500 flex flex-col content-center flex-wrap p-10">
-      <div className="grid grid-cols-4 gap-3 w-full">
+      <div className={`${isClassActive ? 'hflex flex-wrap flex-col content-center justify-center' : 'hidden'}`}>
+        <img src="/baby-shark.gif" className="max-w-60 mx-auto" />
+        <h2 className="text-white text-4xl uppercase font-bold block text-center">Loading... </h2>
+        <h2 className="text-white text-7xl uppercase font-bold block text-center mt-5">{countdown}</h2>
+      </div>
+      <div className={`grid grid-cols-4 gap-3 w-full ${isClassActive ? 'no-touch' : ''}`}>
         {videos.map((video, index) => (
           <div className="drop-shadow-lg flex justify-center content-end flex-wrap border-4 border-blue-700 rounded-xl aspect-square" style={{
             backgroundImage: `url('${index}.jpg')`,
@@ -33,7 +60,7 @@ export default function Home() {
           }} key={index} onClick={() => handleClick(index)}>
             <div className="bg-blue-500/80 border-t-4 border-blue-700 px-3 py-1 flex flex-col justify-center content-center w-full">
               <h2 className="text-center text-white text-md xl:text-3xl">{video[0]}</h2>
-              <span className="text-center font-bold text-white text-xs xl:text-xl">{video.length} Videos</span>             
+              <span className="text-center font-bold text-white text-xs xl:text-xl">{video.length-1} Videos</span>             
             </div>
           </div>
         ))}
